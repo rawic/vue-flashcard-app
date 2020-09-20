5a06e4327f40f50a9564957fc48de64b797da5b5
rawic <rawic.me@gmail.com>
2020-09-20 17:01:56 +0200

    Add readme images

--- src/App.vue
+++ src/App.vue
@@ -41,21 +41,21 @@
 </template>
 
 <script>
-import Search from "./components/Search";
-import AddNewCard from "./components/AddNewCard";
-import Card from "./components/Card";
+import Search from './components/Search';
+import AddNewCard from './components/AddNewCard';
+import Card from './components/Card';
 
-const uuidv4 = require("uuid/v4");
-const colors = ["-orange", "-red", "-purple", "-blue", "-green"];
+const uuidv4 = require('uuid/v4');
+const colors = ['-orange', '-red', '-purple', '-blue', '-green'];
 
 let cards = [
   {
     id: uuidv4(),
-    front: "Who would I like to work with? 🤔",
-    back: "Move Closer! 😎",
+    front: 'Who would I like to work with? 🤔',
+    back: '10Clouds! 😎',
     flipped: false,
     liked: true,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
@@ -65,66 +65,66 @@ let cards = [
       'Mr. Jeffries: "Personally I believe my biggest weakness is in listening." 🤣',
     flipped: false,
     liked: false,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
-    front: "When do I have recruitment interview? ✔️",
-    back: "Tomorrow 📅",
+    front: 'When do I have recruitment interview? ✔️',
+    back: 'Tomorrow 📅',
     flipped: false,
     liked: false,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
-    front: "What is my main goal? 💻",
-    back: "Learn something new and awesome! 🔥",
+    front: 'What is my main goal? 💻',
+    back: 'Learn something new and awesome! 🔥',
     flipped: false,
     liked: true,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
-    front: "Am I ready? 😱",
-    back: "Hell no! 😂",
+    front: 'Am I ready? 😱',
+    back: 'Hell no! 😂',
     flipped: false,
     liked: false,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
-    front: "What is a closure? 😊",
+    front: 'What is a closure? 😊',
     back:
       "A closure is an inner function that has access to the outer (enclosing) function's variables",
     flipped: false,
     liked: false,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
-    front: "Booyah! 🤩",
-    back: "Used when someone is extremely overjoyed 🔥",
+    front: 'Booyah! 🤩',
+    back: 'Used when someone is extremely overjoyed 🔥',
     flipped: false,
     liked: false,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
   },
   {
     id: uuidv4(),
-    front: "Who designed this app? ✏️",
-    back: "Such a modest boy...",
+    front: 'Who designed this app? ✏️',
+    back: 'Such a modest boy...',
     flipped: false,
     liked: false,
-    color: `${colors[Math.floor(Math.random() * colors.length)]}`
-  }
+    color: `${colors[Math.floor(Math.random() * colors.length)]}`,
+  },
 ];
 
 export default {
-  name: "app",
+  name: 'app',
   data() {
     return {
       cards: cards,
-      searchWord: "",
-      likes: cards.filter(card => card.liked).length
+      searchWord: '',
+      likes: cards.filter((card) => card.liked).length,
     };
   },
   methods: {
@@ -135,7 +135,7 @@ export default {
       value ? this.likes++ : this.likes--;
     },
     deleteCard(id) {
-      this.cards = this.cards.filter(card => {
+      this.cards = this.cards.filter((card) => {
         if (card.id === id && card.liked) {
           this.likes--;
         }
@@ -144,22 +144,22 @@ export default {
     },
     addCard(data) {
       this.cards.unshift(data);
-    }
+    },
   },
   computed: {
     filteredData() {
-      return this.cards.filter(card => {
+      return this.cards.filter((card) => {
         return card.front
           .toLowerCase()
           .includes(this.searchWord.trim().toLowerCase());
       });
-    }
+    },
   },
   components: {
     Search,
     AddNewCard,
-    Card
-  }
+    Card,
+  },
 };
 </script>
 
